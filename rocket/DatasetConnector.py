68bb32b2aeef58d892a560f320becd0235454f5e
qdidthis <quirin.ertz@gmail.com>
2022-11-07 21:38:57 +0100

    added padding, changed dataset to df

--- rocket/DatasetConnector.py
+++ rocket/DatasetConnector.py
@@ -1,4 +1,4 @@
-from sktime.datasets import load_UCR_UEA_dataset, load_from_tsfile
+from sktime.datasets import load_UCR_UEA_dataset, load_from_tsfile, load_from_tsfile_to_dataframe
 import numpy as np
 from pathlib import Path, WindowsPath, PosixPath
 import json
@@ -8,7 +8,8 @@ import argparse
 class ClassificationDatasetConnector(argparse.Namespace):
     @property
     def df(self):
-        return load_from_tsfile(Path(self.dataInput), return_data_type='numpy2d')
+        # return load_from_tsfile(Path(self.dataInput), return_data_type='numpy2d')
+        return load_from_tsfile_to_dataframe(Path(self.dataInput))
 
     @property
     def ts(self) -> np.ndarray:
--- rocket/algorithm.py
+++ rocket/algorithm.py
@@ -1,9 +1,12 @@
-from dataclasses import dataclass
-import numpy as np
-import joblib
-from sktime.classification.kernel_based import RocketClassifier
-from DatasetConnector import ClassificationDatasetConnector
 import sys
+from dataclasses import dataclass
+
+import joblib
+import numpy as np
+import pandas as pd
+from DatasetConnector import ClassificationDatasetConnector
+from sktime.classification.kernel_based import RocketClassifier
+from sktime.transformations.panel.padder import PaddingTransformer
 
 
 @dataclass
@@ -15,31 +18,36 @@ class CustomParameters:
 def train(args: ClassificationDatasetConnector):
     data = args.ts
     labels = args.labels
+    padding = PaddingTransformer()
+    data_transformed = padding.fit_transform(data)
     model = RocketClassifier(num_kernels=args.customParameters.num_kernels)
-    model.fit(data, labels)
+    model.fit(data_transformed, labels)
     joblib.dump(model, args.modelOutput)
 
 
 def execute(args: ClassificationDatasetConnector):
     data = args.ts
     model = joblib.load(args.modelInput)
-    scores = model.predict(data)
-    scores = scores.astype(np.uint8)
+    padding = PaddingTransformer()
+    data_transformed = padding.fit_transform(data)
+    scores = model.predict(data_transformed)
+    # scores = scores.astype(np.uint8)
     print(scores)
-    #np.savetxt(args.dataOutput, scores, delimiter=",")
+    scores = pd.factorize(scores, sort=True)[0].astype(np.uint8)
+    # np.savetxt(args.dataOutput, scores, delimiter=",")
     scores.tofile(args.dataOutput, sep="\n")
 
 
 def set_random_state(config: ClassificationDatasetConnector) -> None:
     seed = config.customParameters.random_state
     import random
+
     random.seed(seed)
     np.random.seed(seed)
 
 
 if __name__ == "__main__":
-    args = ClassificationDatasetConnector.from_sys_args(
-        CustomParameters, sys.argv[1])
+    args = ClassificationDatasetConnector.from_sys_args(CustomParameters, sys.argv[1])
     set_random_state(args)
     if args.executionType == "train":
         train(args)
@@ -47,4 +55,5 @@ if __name__ == "__main__":
         execute(args)
     else:
         raise ValueError(
-            f"No executionType '{args.executionType}' available! Choose either 'train' or 'execute'.")
+            f"No executionType '{args.executionType}' available! Choose either 'train' or 'execute'."
+        )
